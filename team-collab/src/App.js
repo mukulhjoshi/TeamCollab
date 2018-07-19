0cf91d31b795eb4504587255f8f779b7c0d0a10f
Mukul Joshi <mukulhjoshi@gmail.com>
2018-07-19 19:31:40 +0530

    fresh

--- team-collab/src/App.js
+++ team-collab/src/App.js
@@ -1,39 +1,21 @@
-import "./SignIn.css";
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { signIn } from "../actions";
-import PropTypes from "prop-types";
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-class Signin extends Component {
-  static contextTypes = {
-    router: PropTypes.object
-  };
-
-  componentWillUpdate(nextProps) {
-    if (nextProps.auth) {
-      this.context.router.history.push("/app");
-    }
-  }
-
+class App extends Component {
   render() {
     return (
-      <div className="row social-signin-container">
-        <div className="col s10 offset-s1 center-align">
-          <img alt="Sign in" id="sign-in" src="/img/user.png" />
-          <h4 id="sign-in-header">Sign In to start</h4>
-          <a href="#" className="social-signin" onClick={this.props.signIn}>
-            <i className="fa fa-google social-signin-icon" />
-            Sign In With Google
-          </a>
-        </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
 }
 
-function mapStateToProps({ auth }) {
-  return { auth };
-}
-
-export default connect(mapStateToProps, { signIn })(Signin);
-view rawSignIn.js hosted with ❤ by GitHub
+export default App;
